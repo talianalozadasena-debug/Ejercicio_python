72dc5c0492dc1542f1df4f439f2ca5bbd866da7f
Taliana Lozada <talianalozadasena.@gmail.com>
2026-06-21 15:00:00 -0500

    Instalación dependencia FastAPi y sqlmodel, configuración del archivo a conexión base de datos, y funcionamiento para crear clientes y listar clientes con la base de datos sqlite.

--- app/enrutadores/clientes.py
+++ app/enrutadores/clientes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from ..modelos.clientes import Cliente, ClienteCrear, ClienteEditar
 from ..listas import lista_clientes
+from ..conexion_bd import Sesion_dependencia
+from sqlmodel import select
 
 rutas_clientes = APIRouter()
 #lista_clientes: list[Cliente] = []
@@ -8,13 +10,17 @@ rutas_clientes = APIRouter()
 
 #endpoint, para obtener o listar todos los clientes
 @rutas_clientes.get("/clientes", response_model=list[Cliente])
-async def listar_clientes():
-    return lista_clientes
+async def listar_clientes(sesion: Sesion_dependencia):
+    lista_cli = sesion.exec(select(Cliente)).all()
+    return lista_cli
 
 
 #endpoint, para obtener o listar un solo cliente de la lista
-@rutas_clientes.get("/clientes/{cliente_id}", response_model=Cliente)
-async def listar_cliente(cliente_id: int):
+@rutas_clientes.get(
+        "/clientes/{cliente_id}", 
+        response_model=Cliente,
+)
+async def listar_cliente(cliente_id: int, mi_sesion: Sesion_dependencia):
     #Recorrer la lista clientes
     for i, obj_cliente in enumerate(lista_clientes):
         if obj_cliente.id == cliente_id:
@@ -26,12 +32,11 @@ async def listar_cliente(cliente_id: int):
 
 #endpoint, para crear un cliente, y agregar a la lista
 @rutas_clientes.post("/clientes", response_model=Cliente)
-async def crear_cliente(datos_cliente: ClienteCrear):
+async def crear_cliente(datos_cliente: ClienteCrear, mi_sesion: Sesion_dependencia):
     cliente_val = Cliente.model_validate(datos_cliente.model_dump())
-    #Generar id
-    id_cliente = len(lista_clientes)+1
-    cliente_val.id = id_cliente
-    lista_clientes.append(cliente_val)
+    mi_sesion.add(cliente_val)
+    mi_sesion.commit()
+    mi_sesion.refresh(cliente_val)
     return cliente_val
 
 
