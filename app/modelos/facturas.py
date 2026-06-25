a7d0886911d4adabaa35f0c9ca7336d520a5e50b
Taliana Lozada <talianalozadasena.@gmail.com>
2026-06-25 00:12:21 -0500

    Finalizacion relación virtual, para presentar la lista de transacciones que corresponden a una factura.

--- app/modelos/facturas.py
+++ app/modelos/facturas.py
@@ -18,18 +18,13 @@ class FacturaBase(SQLModel):
     @computed_field
     @property
     def vr_total(self) -> float:
-        #Calcular (cantidad * vr_unitario)
-        #Consultar el id actual de factura
-        #factura_id_actual = getattr(self, "id", None)
-        #total_factura = 0.0
-        #if not factura_id_actual or not self.transacciones:
-        #    return total_factura
-        #Recorrer la lista de transacciones, según el facutra id
-        #for transaccion in self.transacciones:
-        #    if transaccion.factura_id == factura_id_actual:
-        #        total_factura += transaccion.vr_unitario * transaccion.cantidad
-
-        return 0.0
+        total_factura = 0.0
+        if self.transacciones == None:
+            return total_factura
+        #Recorrer la lista de transacciones, según el factura_id
+        for transaccion in self.transacciones:
+            total_factura += transaccion.vr_unitario * transaccion.cantidad
+        return total_factura
         
 
 class FacturaCrear(FacturaBase):
@@ -43,10 +38,18 @@ class FacturaEditar(FacturaBase):
 class Factura(FacturaBase, table = True):
     id: int | None = Field(default=None, primary_key = True)
     cliente_id: int = Field(default=None, foreign_key = "cliente.id")
-    #Crear relaciones virtuales con cliente - NO en la BD
-    cliente : Cliente = Relationship(back_populates="facturas")
+    #Crear relaciones virtuales con cliente, transacciones - NO en la BD
+    cliente : Cliente = Relationship(back_populates="factura")
+    transacciones: list[Transaccion] = Relationship(back_populates="factura")
+
 
 #Crear modelo para mostrar al usuario o el cliente
 class FacturaLeer(FacturaBase):
     id: int
     cliente: ClienteLeer
+    # Pero no es recomendable, por las buenas prácticas
+    # transacciones: list[Transaccion] = []
+
+
+class FacturaLeerCompuesta(FacturaLeer):
+    transacciones: list[Transaccion] = []
